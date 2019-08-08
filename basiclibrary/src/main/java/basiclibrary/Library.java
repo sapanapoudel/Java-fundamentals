6ad3953d81827cfe113b2f6783775cd5dafb0015
sapanapoudel <sapana.poudel17@outlook.com>
2019-08-08 13:30:27 -0700

    lab3

--- basiclibrary/src/main/java/basiclibrary/Library.java
+++ basiclibrary/src/main/java/basiclibrary/Library.java
@@ -3,9 +3,24 @@
  */
 package basiclibrary;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Library {
+    public static void main(String[] args) {
+        List<String> votes = new ArrayList<>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+
+        String winner = tally(votes);
+        System.out.println(winner + " received the most votes!");
+    }
 
     //Rolling dice
     public static int[] roll(int n) {
@@ -55,6 +70,66 @@ public class Library {
         return minAvgArray;
     }
 
+    //Maps
+//    public static void main(String[] args) {
+//        HashMap<String, Integer[]> animalFriendliness = new HashMap<>();
+//        animalFriendliness.put("Ginger", new Integer[]{11});
+//        animalFriendliness.put("that raccoon in my trash can", new Integer[]{0});
+//        animalFriendliness.put("all of the student", new Integer[]{9,9,9,9,9,9,9,9,9,9,9,9,9,9});
+//
+//        System.out.println(animalFriendliness.get("Ginger"));
+//    }
+    public static String weatherData(int[][] arrs) {
+        String finalInput = " ";
+        HashSet<Integer> h = new HashSet<>();
+        for(int i = 0; i < arrs.length; i++) {
+            for(int item : arrs[i]) {
+                h.add(item);
+            }
+        }
+        int minValue = Integer.MAX_VALUE;
+        int maxValue = Integer.MIN_VALUE;
+        for(int value : h){
+            if (value > maxValue){
+                maxValue = value;
+                System.out.println("High: " + maxValue);
+            } else if (value < minValue) {
+                minValue = value;
+                System.out.println("Low: " + minValue);
+            }
+        }
 
+        StringBuilder string = new StringBuilder(" ");
+        for(int i =  minValue; i < maxValue; i++) {
+
+            if (!h.contains(i)) {
+                string.append(i);
+                System.out.println("Never saw temperature: " + i);
+            }
+        }
+        return string.toString();
+        //return "High";
+
+
+    }
+
+    // Tallying Election
+    public static String tally(List<String> list) {
+        HashMap < String, Integer> vote = new HashMap<>();
+        for(String item : list) {
+            vote.put(item, 0);
+        }
+        int max = 0;
+        String maxVotes = " ";
+        for(String item : list) {
+            if(vote.get(item) > max) {
+                max = vote.get(item) + 1;
+                maxVotes = item;
+            }
+            vote.put(item, max);
+        }
+
+        return maxVotes;
+    }
 
 }
