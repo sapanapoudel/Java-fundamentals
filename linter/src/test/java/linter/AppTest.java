650821093373c20759995b99e6b98b62cf149838
sapanapoudel <sapana.poudel17@outlook.com>
2019-08-11 07:35:14 -0700

    more test

--- linter/src/test/java/linter/AppTest.java
+++ linter/src/test/java/linter/AppTest.java
@@ -4,10 +4,13 @@
 package linter;
 
 import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
+    @Test public void testAppHasAGreeting() throws FileNotFoundException {
         App classUnderTest = new App();
         String PATH = "/Users/pousapan/codefellows-401/java-fundamentals!/linter/src/main/resources/gates.js";
         String expected = "Line 3: Missing semicolon.";
@@ -16,9 +19,24 @@ public class AppTest {
     }
 
     //Test for file with no error
-//    @Test public void readFileTest_noError() {
-//        App classUnderTest = new App();
-//        String PATH = "/Users/pousapan/codefellows-401/java-fundamentals!/linter/src/main/resources/contains_error.js";
-//        assertEquals("", "no error", classUnderTest.readFile(PATH));
-//    }
+    @Test public void readFileTest_noError() throws FileNotFoundException {
+        App classUnderTest = new App();
+        String PATH = "/Users/pousapan/codefellows-401/java-fundamentals!/linter/src/main/resources/contains_error.js";
+        assertEquals("There should be no error", "Line 7: has no error",
+                classUnderTest.readFile(PATH));
+    }
+
+    @Test public void readFileTest_fewError() throws FileNotFoundException {
+        App classUnderTest = new App();
+        String PATH = "/Users/pousapan/codefellows-401/java-fundamentals!/linter/src/main/resources/few_error.js";
+        assertEquals("There should be missing semicolon", "Line 3: Missing semicolon.",
+                classUnderTest.readFile(PATH));
+    }
+
+    @Test public void readFileTest_emptyFile() throws FileNotFoundException {
+        App classUnderTest = new App();
+        String PATH = "/Users/pousapan/codefellows-401/java-fundamentals!/linter/src/main/resources/empty_file.js";
+        assertEquals("File is empty", "file is empty",
+                classUnderTest.readFile(PATH));
+    }
 }
